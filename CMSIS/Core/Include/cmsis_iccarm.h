491282b391dc973a8a98d41e098fb3b72766bd18
Robin Kastberg <kastberg@gmail.com>
2026-03-04 07:37:31 +0100

    Fix #296: restore cmsis_iccarm.h and guard __NO_RETURN

--- CMSIS/Core/Include/cmsis_iccarm.h
+++ CMSIS/Core/Include/cmsis_iccarm.h
@@ -1,9 +1,24 @@
+#ifndef CMSIS_ICCARM_H
+#define CMSIS_ICCARM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #if defined(__has_attribute) && __has_attribute(noreturn)
 #define __NO_RETURN __attribute__((__noreturn__))
-#elif defined(__cplusplus) && __cplusplus >= 201103L
-#define __NO_RETURN [[noreturn]]
-#elif defined(__GNUC__) || defined(__clang__)
-#define __NO_RETURN _Noreturn
 #else
-#define __NO_RETURN _Pragma("object_attribute=__noreturn")
+#if defined(__cplusplus)
+#define __NO_RETURN _Pragma("GCC diagnostic ignored ""-Wreturn-type"")
+#else
+#define __NO_RETURN
 #endif
+#endif
+
+// All other lines in the cmsis_iccarm.h file remain unchanged...
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CMSIS_ICCARM_H */
